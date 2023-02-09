999dd2507610f910044b509935f4a8dcd1373a80
Brandon Castro <castroyou999@gmail.com>
2023-02-08 22:18:37 -0500

    test deploy

--- bin/recipes-build.sh
+++ bin/recipes-build.sh
@@ -2,6 +2,12 @@
 # exit on error
 set -o errexit
 
+# Add build commands for front end
+rm -rf public
+npm install --prefix client && npm run build --prefix client
+cp -a client/build/. public/
+
 bundle install
-bundle exec rake db:migrate 
-bundle exec rake db:seed
+bundle exec rake db:migrate
+
+# bundle exec rake db:seed
